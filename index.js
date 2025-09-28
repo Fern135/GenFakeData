fef9168609490058069cdf7e93d175e6e88ef3d2
Fernando Camblor <fernandocamblor@fernandos-mbp.lan>
2025-09-27 20:18:26 -0400

    tried to make it work somehow

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use((req, res, next) => {
 
 // logging every request
 app.use((req, res, next) => {
-    console.log(`Incoming Request: ${req.method} = ${req.url}\t${req.statusCode}`);
+    console.log(`${new Date().toISOString()}: Incoming Request: ${req.method} ${req.url}`);
     next();
 });
 
